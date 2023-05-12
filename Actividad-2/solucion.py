fddfd0cd23db06645e125d2d1be5047a9204ffec
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-05-12 17:22:29 -0300

    Tester

--- Actividad-2/solucion.py
+++ Actividad-2/solucion.py
@@ -1,22 +1,41 @@
-n, W, L = map(int, input().split())
-expansions = [(i+1,) + tuple(map(int, input().split())) for i in range(n)]
-expansions.sort(key=lambda x: x[1], reverse=False)  # Ordenar por largo
+import heapq
 
-applied_expansions = []
-expansiones_aplicadas = []
-for i in range(n):
-    w, l = expansions[i][1:]
-    if w > W and l > L:
-        applied_expansions.append(expansions[i][0])
-        W, L = w, l
+def main():
+    n, W, L = map(int, input().split())
+    expansion_plans = [tuple(map(int, input().split())) + (i + 1,) for i in range(n)]
 
-for j in range(n):
-    w, l = expansions[1:][j]
-    if w > W and l > L:
-        applied_expansions.append(expansions[j][0])
-        W, L = w, l
+    # Filtrar planes de expansión válidos
+    valid_plans = [(w, l, i) for w, l, i in expansion_plans if w > W and l > L]
 
+    # Ordenar planes de expansión por ancho y luego por largo
+    valid_plans.sort(key=lambda x: (x[0], x[1]))
 
-#print(len(applied_expansions))
-print(*applied_expansions)
-print(*expansiones_aplicadas)
+    # Aplicar Patience Sort con cola de prioridad
+    piles = []
+    for plan in valid_plans:
+        idx = find_pile(piles, plan)
+        if idx == -1:
+            heapq.heappush(piles, [plan[1], [plan[2]]])
+        else:
+            heapq.heappush(piles[idx][1], plan[2])
+            piles[idx][0] = plan[1]
+
+    # Encontrar la pila con la mayor cantidad de planes de expansión
+    if piles:
+        max_pile = max(piles, key=lambda x: len(x[1]))
+        print(len(max_pile[1]))
+        print(*max_pile[1])
+    else:
+        print(0)
+
+def find_pile(piles, plan):
+    idx = -1
+    min_diff = float('inf')
+    for i, pile in enumerate(piles):
+        if pile[0] < plan[1] and plan[1] - pile[0] < min_diff:
+            idx = i
+            min_diff = plan[1] - pile[0]
+    return idx
+
+if __name__ == "__main__":
+    main()
---
+++ Actividad-2/tester.py
@@ -0,0 +1,29 @@
+import subprocess
+
+test_file = "test-1.txt"
+
+# Abre el archivo de prueba
+with open(test_file, 'r') as f:
+    lines = f.readlines()
+
+# Separa las líneas en input y output esperado
+input_lines = []
+expected_output = []
+for line in lines:
+    if line.strip() == '-':
+        break
+    input_lines.append(line)
+for line in lines[len(input_lines)+1:]:
+    expected_output.append(line.strip())
+
+# Ejecuta el script de solución y envíale el input del archivo de prueba
+p = subprocess.Popen(['python', 'solucion.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+input_str = ''.join(input_lines)
+output_str = p.communicate(input=input_str.encode())[0].decode()
+
+# Compara la salida del script de solución con el output esperado
+if output_str.strip().split('\n') == expected_output:
+    print(f"{test_file} superado exitosamente.")
+else:
+    # imprime el mensaje de error si hay una discrepancia
+    print(f"Error en {test_file}\nOutput de la solucion:\n{output_str}\n--------------------------------------------")
